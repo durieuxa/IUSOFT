cf3f531af3e49d92110b147ab3e1dafa10c838ae
georgela <georgela@PC-B123-12.iut-valence.fr>
2016-04-28 12:49:09 +0200

    Initial commit basic card structure

---
+++ src/card.java
@@ -0,0 +1,23 @@
+
+public class card
+{
+	private final String cardName;
+	private final int cardRef;
+	private final int cardID;
+	
+	public card(String name, int ref, int id){
+		this.cardName = name;
+		this.cardRef = ref;
+		this.cardID = id;
+	}
+		
+	public boolean checkEffect(){
+		//TODO
+		return false;
+	}
+		
+	private void effect(){
+		//TODO 
+	}
+		
+}
